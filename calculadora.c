48e3e279a202b2aafba38a839a62d9eddbf86bb0
thousandeyess <136214474+thousandeyess@users.noreply.github.com>
2024-02-22 13:20:43 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+include <stdio.h>
 
-int main(){
-  printf("Hola Mundo");
-  return 0;
+
+int main(void){
+    double numero1, numero2;
+    char operador;
+  
+    printf("Ingresa el primer valor: ");
+    scanf("%lf", &numero1);
+    printf("Ingresa el signo de la operacion: ");
+    scanf(" %c", &operador);
+    printf("Ingresa el segundo valor: ");
+    scanf("%lf", &numero2);
+
+    if(operador == '+'){
+        printf("La suma de los dos valores es: %0.2lf\n", (numero1+numero2));
 }
-
-int multiplicacion(int A, int B){
-  int mult=0;
-  mult=(A*B);
-
-  return mult;
-}
-
-int division(int A, int B){
-  int rest=0;
-  if(B==0) return 0;
-  return A/B;
+    return 0;
 }
