e7a8b9a23efac32c96585c7aaf070a5ede7ad5ba
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2024-02-23 19:20:18 -0600

    menu calculadora agregado

--- calculadora.c
+++ calculadora.c
@@ -11,20 +11,36 @@ int main(){
     printf("Ingresa el segundo valor: ");
     scanf("%lf", &numero2);
 
-    if(operador == '+')
+
+    switch(operador){
+      case '+':
         printf("La suma de los dos valores es: %0.2lf\n", (numero1+numero2));
+        break;
+      case '-':
+        printf("La resta de los dos valores es: %0.2lf\n", 2);
+        break;
+      case '*':
+        printf("La multiplicación de los dos valores es: %0.2lf\n", multiplicacion(numero1,numero2));
+        break;
+      case '/':
+        printf("La división de los dos valores es: %0.2lf\n", division(numero1,numero2));
+        break;
+      default:
+        printf("Opción invalida.");
+        break;
+    }
     
   return 0;
 }
 
-int multiplicacion(int A, int B){
-  int mult=0;
+double multiplicacion(double A, double B){
+  double mult=0;
   mult=(A*B);
   return mult;
 }
 
-int division(int A, int B){
-  int rest=0;
+double division(double A, double B){
+  double rest=0;
   if(B==0) return 0;
   return A/B;
 }
